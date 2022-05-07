282f070cb1bbe4884b50efeec423d3028b6b5738
Yuyan-C <chenyuyan@Yuyans-MacBook-Air.local>
2022-05-07 11:10:53 -0400

    feat: preprocess

---
+++ src/data_preprocess.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+
+
+def read_data(path):
+    df = pd.read_csv(path)
+    y = df.pop('nEvent')
+    X = df
+    return X, y
+
+
+def encode(df):
+    label_encoder = preprocessing.LabelEncoder()
+    df['diet'] = label_encoder.fit_transform(df['diet'])
+    for num in "1234":
+        df[f'MachineryVar{num}'] = label_encoder.fit_transform(df[f'MachineryVar{num}'])
+    return df
+
+
+def preprocess_na(df, method):
+    if method == "zero":
+        df = df.fillna(0)
+    if method == "mean":
+        df = df.fillna(df.mean())
+    return df
+
+
+def training_data():
+    path = "../data/data.csv"
+    X, y = read_data(path)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train = preprocess_na(encode(X_train), method="zero").to_numpy()
+    X_test = preprocess_na(encode(X_test), method="zero").to_numpy()
+    y_train = y_train.to_numpy()
+    y_test = y_test.to_numpy()
+    return X_train, X_test, y_train, y_test
+
+
+def validation_data():
+    path = "../data/data.csv"
+    X = pd.read_csv(path)
+    X = preprocess_na(encode(X), method="zero").to_numpy()
+    return X
+
+
---
+++ src/main.py
@@ -0,0 +1,5 @@
+from data_preprocess import *
+
+if __name__ == "__main__":
+    X_train, X_test, y_train, y_test = training_data()
+    print(X_train)
